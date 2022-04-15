8d09629777d0b253603e9f0d1d74c2a1efe45feb
JOhn V <johndominicvillalino@outlook.com>
2022-04-15 21:36:44 +0800

    add winner logic

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-import { createDivElements,createDivContainer } from "./helper.js";
+import { createDivElements, createDivContainer,playerMoves,calculateWin} from "./helper.js";
 
-const dom = document
-const root = dom.getElementById('root')
-
-window.addEventListener('load', () => {
-    const  div = createDivElements()
-
-    //init board
-    const container = createDivContainer('gridContainer','board')
-    root.appendChild(container)
-    div.forEach(e => {
-        dom.getElementById('board').appendChild(e)
-    })
+const dom = document;
+const root = dom.getElementById("root");
 
 
+//start on load
+window.addEventListener("load", () => {
+  const div = createDivElements();
 
-    
-})
+  //init board
+  const container = createDivContainer("gridContainer", "board");
+  root.appendChild(container);
+  div.forEach((e) => {
+    dom.getElementById("board").appendChild(e);
+  });
 
+  const tictacCell = dom.querySelectorAll(".tictacCell");
+
+  let player = 0;
+  tictacCell.forEach((e) => {
+    e.addEventListener("click", (el) => {
+      let text;
+      const boxNum = el.target.getAttribute("box");
+      const boxPos = el.target.getAttribute("boxpos");
+
+      if(dom.getElementById(el.target.id).textContent.length > 0){
+        return
+      } else {
+        player +=1;
+        if(player % 2 === 0) {
+          player = 2;
+          text = 'O'
+        } else {
+          player = 1
+          text = 'X'
+        }
+        
+        dom.getElementById(el.target.id).textContent = text
+        playerMoves([boxPos,boxNum],player)
+        calculateWin(player)
+      }
+    });
+  });
+
+  // end on load
+});
--- helper.js
+++ helper.js
@@ -1,14 +1,44 @@
 
+import {getStorageData,uploadStageData} from './util.js'
+
 const dom = document
+const storage = window.localStorage
 
 export const createDivElements = () => {
     const divsArr = []
     for (let i = 0; i < 9; i++) {
      const div = dom.createElement('div')
-        div.setAttribute('id',i+1)
-        div.setAttribute('class','tiktokCell')
+        const exactPos = i+1;
+        let boxNum;
+        let boxPos; 
+        //cell ids anjd position
+        if(exactPos % 3 === 0) {
+            boxNum = 3
+        } else {
+            boxNum = exactPos % 3
+        }   
+        if(exactPos - boxNum === 0) {
+            boxPos = 1
+        } 
+        if(exactPos - boxNum === 3) {
+            boxPos = 2
+        } 
+        if(exactPos - boxNum === 6) {
+            boxPos = 3
+        } 
+
+        div.setAttribute('id',`cell${i+1}`)
+        div.setAttribute('box',`${boxNum}`)
+        div.setAttribute('boxpos',`${boxPos}`)
+        
+        div.setAttribute('class','tictacCell')
         divsArr.push(div)
     }   
+
+    storage.setItem('playerOne',JSON.stringify([]))
+    storage.setItem('playerTwo',JSON.stringify([]))
+    storage.setItem('board',JSON.stringify([]))
+
     return divsArr;
 }
 
@@ -18,3 +48,57 @@ export const createDivContainer = (classes,id) => {
      div.setAttribute('id',id)
      return div
 }
+
+
+export const playerMoves = (boxNum, player) => {
+    let storagePlayerUpdate; 
+    if(player === 1) {
+        storagePlayerUpdate = 'playerOne'
+    }else {
+        storagePlayerUpdate = 'playerTwo'
+    }
+    //upload playermoves
+    uploadStageData(storagePlayerUpdate,boxNum)
+    
+}
+
+export const calculateWin = (player) => {
+    
+    let checkPlayer; 
+    if(player === 1) {
+        checkPlayer = 'playerOne'
+    }else {
+        checkPlayer = 'playerTwo'
+    }
+    const currentMoves = getStorageData(checkPlayer);
+
+   const board = [[],[],[]]
+   
+    currentMoves.forEach(e => {
+        const v = e[0] -1
+        const h = e[1]
+        board[v].push(h)
+    })
+
+    checkBoard()
+
+    function checkBoard() {
+
+        board.forEach((e) => {
+            
+            const sorted = e.sort()
+            const joined = +sorted.join("")
+            console.log(joined)
+    
+        })
+
+    }
+
+    
+
+
+}
+
+
+
+
